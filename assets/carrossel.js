7c0c2db0d69daefb0ca311abf30edca7e153257b
Paulo Sergio Olivio <95197538+pauloolivio@users.noreply.github.com>
2023-09-25 13:38:54 -0300

    Add files via upload

--- assets/carrossel.js
+++ assets/carrossel.js
@@ -4,11 +4,47 @@ document.addEventListener('DOMContentLoaded', function() {
     var prev = document.querySelector('.gallery__prev');
     var next = document.querySelector('.gallery__next');
 
+    var currentIndex = 0;
+    var interval;
+
+    function goToNext() {
+        currentIndex = (currentIndex + 1) % items.length;
+        stream.appendChild(items[currentIndex]);
+    }
+
+    function startAutoAdvance() {
+        interval = setInterval(goToNext, 2500); // Avança a cada 2.5 segundos
+    }
+
+    function stopAutoAdvance() {
+        clearInterval(interval);
+    }
+
     prev.addEventListener('click', function() {
-        stream.insertBefore(items[items.length - 1], items[0]);
+        currentIndex = (currentIndex - 1 + items.length) % items.length;
+        stream.insertBefore(items[currentIndex], items[0]);
+        stopAutoAdvance();
     });
 
     next.addEventListener('click', function() {
-        stream.appendChild(items[0]);
+        goToNext();
+        stopAutoAdvance();
     });
+
+    prev.removeEventListener('click', null);
+    next.removeEventListener('click', null);
+    // Inicia
+    startAutoAdvance();
+
+    // Pausa 
+    stream.addEventListener('mouseenter', function() {
+        stopAutoAdvance();
+    });
+
+    // Retoma 
+    stream.addEventListener('mouseleave', function() {
+        startAutoAdvance();
+    });
+
+
 });
